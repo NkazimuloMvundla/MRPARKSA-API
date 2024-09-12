813b54d5c38a36c925c8d0299bb600bbc3f5d2d5
Nkazimulo J Mvundla <Nkazimulojudgement@gmail.com>
2024-09-12 23:36:31 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -65,7 +65,7 @@ Route::post('/deleteAllUsers', [RegisteredUserAPIController::class, 'deleteAllUs
 
 //user-parking-routes
 // Route::post('/send-notification', [NotificationController::class, 'sendNotification']);
-Route::post('/send-notification', [NotificationController::class, 'sendNotification']);
+Route::post('/send-notification', [NotificationController::class, 'sendNotification'])->middleware('auth:sanctum');
 Route::get('/find-hourly-parking', [ParkingController::class, 'findHourlyParking'])->middleware('auth:sanctum');
 Route::get('/find-airport-parking', [ParkingController::class, 'findAirportParking'])->middleware('auth:sanctum');
 Route::get('/parking-space/{id}', [ParkingController::class, 'getParkingSpace'])->middleware('auth:sanctum');
