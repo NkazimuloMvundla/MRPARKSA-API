16ff5d17090af78c81866ad5ec5ecd87deaaa2fa
Nkazimulo J Mvundla <Nkazimulojudgement@gmail.com>
2024-09-13 00:25:28 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -54,6 +54,7 @@ Route::middleware([EnsureFrontendRequestsAreStateful::class])->group(function ()
 // ->middleware('guest')
 // ->name('search-parking');
 Route::get('/test', function () {
+    echo asset('storage/mrparksa-bf841-firebase-adminsdk-erp7l-0a5c129d75.json');
     $firebaseCredentialsPath = storage_path('app/firebase/mrparksa-bf841-firebase-adminsdk-erp7l-0a5c129d75.json');
 
     // Check if the path is set and file exists
