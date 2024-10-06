46d72b2a6113d3b8152d065218370178c69ee768
Nkazimulo J Mvundla <Nkazimulojudgement@gmail.com>
2024-10-06 11:25:19 +0200

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -30,7 +30,7 @@ return [
         'http://localhost',
         'http://192.168.0.179',
         'http://192.168.0.179:8000',
-        'https://within-characterization-printers-grad.trycloudflare.com'
+        'https://huntington-harassment-corrected-behavioral.trycloudflare.com'
     ],
 
     'allowed_origins_patterns' => [],
