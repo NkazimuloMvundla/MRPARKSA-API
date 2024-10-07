6e86e6a8d22d571fe462266c999fdb982b029efa
Nkazimulo J Mvundla <Nkazimulojudgement@gmail.com>
2024-10-07 18:59:19 +0200

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -30,7 +30,7 @@ return [
         'http://localhost',
         'http://192.168.0.179',
         'http://192.168.0.179:8000',
-        'https://differential-mesa-removal-journalists.trycloudflare.com'
+        'https://charity-subaru-desert-suit.trycloudflare.com'
     ],
 
     'allowed_origins_patterns' => [],
