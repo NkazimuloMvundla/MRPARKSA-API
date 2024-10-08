cb9e02dfd4da2d242a4a2f49493be533f051bbbb
Nkazimulo J Mvundla <Nkazimulojudgement@gmail.com>
2024-10-08 19:16:17 +0200

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -30,7 +30,7 @@ return [
         'http://localhost',
         'http://192.168.0.179',
         'http://192.168.0.179:8000',
-        'https://charity-subaru-desert-suit.trycloudflare.com'
+        'https://fleet-treated-american-eye.trycloudflare.com '
     ],
 
     'allowed_origins_patterns' => [],
